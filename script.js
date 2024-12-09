0ce015d7a672d12db5da84653677dd498086c12f
Nataliia Kozlitina <kozlitinanatalia.e@gmail.com>
2024-12-09 20:20:24 +0300

    add one more api and api key

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiKey1 = "327540b38b7o0at269e72457c8af0e5b";
 
 function formatDate(timestamp) {
   return `${formatDay(timestamp)} ${formatTime(timestamp)}`;
@@ -31,7 +32,9 @@ function lessThanTen(unit) {
 }
 
 function displayForecast(response) {
+  console.log(response);
   let forecast = response.daily;
+  console.log(forecast);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
@@ -41,26 +44,24 @@ function displayForecast(response) {
         `
         <div class="col-6">
           <span class="weather-forecast-date">
-          ${formatDay(forecastDay.dt)}
+          ${formatDay(forecastDay.time)}
           </span>
         </div>
           <div class="col-2">
             <span class=" weather-forecast-temperatures weather-forecast-temperature-min"> ${Math.round(
-              forecastDay.temp.min
+              forecastDay.temperature.minimum
             )}° </span>
             </div>
             <div class="col-2">
             <img
-              src="http://openweathermap.org/img/wn/${
-                forecastDay.weather[0].icon
-              }@2x.png"
+              src="${forecastDay.condition.icon_url}"
               alt=""
               width="42"
             />
             </div>
             <div class="col-2">
             <span class="weather-forecast-temperatures weather-forecast-temperature-max"> ${Math.round(
-              forecastDay.temp.max
+              forecastDay.temperature.maximum
             )}° </span>
             </div>
     `;
@@ -71,8 +72,29 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
+const codeMapping = {
+  "01d": "clear-sky-day",
+  "01n": "clear-sky-night",
+  "02d": "few-clouds-day",
+  "02n": "few-clouds-night",
+  "03d": "scattered-clouds-day",
+  "03n": "scattered-clouds-night",
+  "04d": "broken-clouds-day",
+  "04n": "broken-clouds-night",
+  "09d": "shower-rain-day",
+  "09n": "shower-rain-night",
+  "10d": "rain-day",
+  "10n": "rain-night",
+  "11d": "thunderstorm-day",
+  "11n": "thunderstorm-night",
+  "13d": "snow-day",
+  "13n": "snow-night",
+  "50d": "mist-day",
+  "50n": "mist-night",
+};
+
 function displayHourlyForecast(response) {
-  let forecastData = response.hourly;
+  let forecastData = response.list;
   let pageSize = 6;
   let currentPage = 1;
 
@@ -92,14 +114,13 @@ function displayHourlyForecast(response) {
             forecastHour.dt
           )}</div>
           <img
-            src="http://openweathermap.org/img/wn/${
-              forecastHour.weather[0].icon
-            }@2x.png"
-            alt=""
+          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+            codeMapping[forecastHour.weather[0].icon]
+          }.png" alt=""
             width="42"
           />
           <div class="weather-forecast-temperatures text-center">
-             ${Math.round(forecastHour.temp)}°
+             ${Math.round(forecastHour.main.temp)}°
           </div>
         </div>
     `;
@@ -132,11 +153,12 @@ function displayHourlyForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  fetch(apiUrl)
+  let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiSheCodes = `https://api.shecodes.io/weather/v1/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&key=${apiKey1}&units=metric`;
+  fetch(apiSheCodes)
     .then((response) => response.json())
     .then((response) => displayForecast(response));
-  fetch(apiUrl)
+  fetch(api)
     .then((response) => response.json())
     .then((response) => displayHourlyForecast(response));
 }
@@ -166,7 +188,9 @@ function displayTemperature(response) {
   sunsetElement.innerHTML = formatTime(response.sys.sunset);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+      codeMapping[response.weather[0].icon]
+    }.png`
   );
   iconElement.setAttribute("alt", response.weather[0].description);
 
